18a98fb063f432293f1e9011d96ac614e778b647
mikeyjcat <mike@hewitson.co.za>
2016-05-01 06:18:03 +0200

    Add aggregation to history query

--- dbscripts/aggregation.js
+++ dbscripts/aggregation.js
@@ -11,3 +11,50 @@ db.readings.aggregate(
 
 https://api.forecast.io/forecast/62888a9ff1907377b60a866701cf3338/-26.12431498586955,28.027605633880835
 
+       var query = Readings.aggregate([{
+            $match: {
+                date: { $gte: dateFrom }
+            }
+        }, {
+            $unwind: {
+                path: "$sensors"
+            }
+        }, {
+            "$match": {
+                "sensors.sensor": { "$eq": "Ambient" }
+            }
+        }, {
+            $project: {
+                sensors: 1,
+                yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } }
+            }
+        }, {
+            "$group": {
+                "_id": {
+                    "date": "$yearMonthDay"
+                },
+                "count": {
+                    "$sum": 1
+                },
+                "avgHum": {
+                    "$avg": "$sensors.hum"
+                },
+                "avgTemp": {
+                    "$avg": "$sensors.temp"
+                },
+                "maxTemp": {
+                    "$max": "$sensors.temp"
+                },
+                "minTemp": {
+                    "$min": "$sensors.temp"
+                },
+                "maxHum": {
+                    "$max": "$sensors.hum"
+                },
+                "minHum": {
+                    "$min": "$sensors.hum"
+                }
+            }
+        }, {
+            $sort: { "_id.date": 1}
+        }]);
--- routes/historyRouter.js
+++ routes/historyRouter.js
@@ -18,14 +18,34 @@ historyRouter.route('/')
         var dateTo;
         var dateFrom = new Date();
         dateTo = new Date(Date.now());
+
         dateFrom.setDate(dateTo.getDate() - 7);
-        var query = Readings.find({
-                date: {
-                    $gte: dateFrom,
-                    $lt: dateTo
-                }
-            })
-            .sort({ date: 1 });
+        var query = Readings.aggregate([{
+            $match: {
+                date: { $gte: dateFrom }
+            }
+        }, {
+            $project: {
+                sensors: 1,
+                date: 1,
+                theMod: { $mod: [{ $millisecond: "$date" }, 7] }
+            }
+        }, {
+            $match: {
+                theMod: { $eq: 0 }
+            }
+        }, {
+            $sort: { date: 1 }
+        }]);
+
+        // var query = Readings.find({
+        //         date: {
+        //             $gte: dateFrom,
+        //             $lt: dateTo
+        //         }
+        //     })
+        //     .sort({ date: 1 });
+
         query.exec(function(err, reading) {
             if (err) throw err;
             res.json(reading);
